515a39789edb390246c086d0d6c015407c28098b
Mateusz Jankowski <matijank10@gmail.com>
2021-12-23 08:51:16 +0100

    Generate Library model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_074722) do
+ActiveRecord::Schema.define(version: 2021_12_23_075045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2021_12_23_074722) do
     t.string "title"
     t.text "description"
     t.string "author"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.integer "book_id"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

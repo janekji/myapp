5ea5a4acf1f9b12cd3485b4450c8342b713a6b75
Mateusz Jankowski <matijank10@gmail.com>
2021-12-23 10:04:12 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :books
-  root 'books#index'
+  resources :books do
+    member do
+      put "add", to: "books#library"
+      put "remove", to: "books#library"
+    end
+  end
+  resources :library
   devise_for :users
+  root 'books#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
-
-
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

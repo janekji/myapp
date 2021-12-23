937798c4468420811f54b9153a327ab139824563
Mateusz Jankowski <matijank10@gmail.com>
2021-12-23 08:58:17 +0100

    Before_action fix to let guests check books but only logged in user can do other actions
    Add and remove function for current user for library

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,73 @@
-class BooksController < InheritedResources::Base
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy, :library]
+  before_action :authenticate_user!, expect: [:index, :show]
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @book = current_user.books.build
+  end
+
+  def edit
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+
+    respond_to do |f|
+      if @book.save
+        f.html {redirect_to @book, notice: "Książka zostałą pomślnie dodana"}
+      else
+        f.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |f|
+      if @book.update(book_params)
+        f.html { redirect_to @book, notice: "Książka zostałą pomyśle zaktualizowana"}
+      else
+        f.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |f|
+      f.html { redirect_to books_url, notice: "Książka została usunięta"}
+    end
+  end
+
+  def library
+    type = params[:type]
+
+    if type == "add"
+      current_user.library_additions << @book
+      redirect_to library_index_path, notice: "#{@book.title} została dodana z biblioteki"
+    elsif type == "remove"
+      current_user.library_additions.delete(@book)
+      redirect_to root_path, notice: "#{@book.title} została usunięta z biblioteki"
+    else
+      redirect_to book_path(@book), notice: "Coś poszło nie tak. Sprobuje jeszcze raz"
+    end
+  end
 
   private
 
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
     def book_params
-      params.require(:book).permit(:title, :description, :author, :user_id)
+      params.require(:book).permit(:title, :description, :author)
     end
 
 end

9258ec078653fd5db8ae67bb315282e91752acf1
Mateusz Jankowski <matijank10@gmail.com>
2021-12-27 09:15:30 +0100

    Fix a typo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy, :library]
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, expcept: [:index, :show]
   
   def index
     @books = Book.all

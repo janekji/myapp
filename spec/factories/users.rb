6ae3f63ae0c46cfbbe2fb537b4609c7eef2f69e4
Mateusz Jankowski <matijank10@gmail.com>
2021-12-27 13:08:43 +0100

    Add factorie for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory(:user) do
+    email { "test.a@test.com" }
+    password { "SecretPassword123" }
+    password_confirmation { "SecretPassword123" }
   end
 end

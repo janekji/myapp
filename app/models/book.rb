23315595fa45773e2d44e85d0ad2fb2b3f9bb63e
Mateusz Jankowski <matijank10@gmail.com>
2021-12-29 10:49:11 +0100

    Add aasm state for book models and implement code to create books for standard users

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,16 @@
 class Book < ApplicationRecord
+  include AASM
+
   belongs_to :user
   has_many :libraries
   has_many :added_books, through: :libraries, source: :user
+
+  aasm do
+    state :no_status, initial: true
+    state :complete
+  
+    event :complete do
+      transitions from: :no_status, to: :complete
+    end
+  end
 end
